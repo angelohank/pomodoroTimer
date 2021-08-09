98d50bfd7f7d81d4f2fa738390f4a3adcea93e51
littleMen21 <angelohank@alunos.utfpr.edu.br>
2021-08-09 08:04:37 -0300

    2

--- js/data-hora.js
+++ js/data-hora.js
@@ -1,11 +1,23 @@
 var data = new Date();
+
+setInterval(function atualizaHoraEmTempoReal(){
+    
 var hora = data.getHours().toString().padStart(2, '0');
 var minuto = data.getMinutes().toString().padStart(2, '0');
-var segundos = data.getSeconds().toString().padStart(2, '0');;
+var segundos = data.getSeconds().toString().padStart(2, '0');
 
 var campo_hora = document.querySelector(".campo_hora");
-var botao = document.querySelector("#botao");
+campo_hora.textContent = hora + ':' + minuto + ':' + segundos;
+    
+}, 1);
 
-botao.addEventListener("click", function(){
-    campo_hora.textContent = (hora + ':' + minuto + ':' + segundos);
-})
+
+var dia = data.getDay().toString().padStart(2, '0');
+var mes = data.getMonth().toString().padStart(2, '0');
+var ano = data.getFullYear();
+
+
+var campo_data = document.querySelector(".campo_data");
+campo_data.textContent = (dia + '/' + mes + '/' + ano);
+
+
