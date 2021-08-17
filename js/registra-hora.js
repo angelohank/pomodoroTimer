2dfbbf1d867836f5524059152128eb25edafa7f8
littleMen21 <angelohank@alunos.utfpr.edu.br>
2021-08-17 16:57:11 -0300

    ajustes

--- js/registra-hora.js
+++ js/registra-hora.js
@@ -4,5 +4,4 @@ botao.addEventListener("click", function(){
     botao.textContent = '';
     botao.append("Horário registrado com sucesso!");
     botao.classList.remove("is-outlined");
-    botao.setAttribute('disabled', false);
 })
