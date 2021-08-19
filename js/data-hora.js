f9dc34842f9ff897b5911d7c4d43a292f76bdc59
littleMen21 <angelohank@alunos.utfpr.edu.br>
2021-08-19 16:58:21 -0300

    padraoDataEHora

--- js/data-hora.js
+++ js/data-hora.js
@@ -1,22 +1,37 @@
-setInterval(atualizaHoraEmTempoReal, 1000);
+setInterval(atualizaHoraEmTempoReal, 1);
 
 function atualizaHoraEmTempoReal(){
  
-    const hora = new Date();    
+    var hora = new Date();    
+    
     var hora_atual = hora.getHours().toString().padStart(2, '0');
     var minuto = hora.getMinutes().toString().padStart(2, '0');
     var segundos = hora.getSeconds().toString().padStart(2, '0');
 
-    var campo_hora = document.querySelector(".campo_hora");
+    const campo_hora = document.querySelector(".campo_hora");
     campo_hora.innerHTML = hora_atual + ':' + minuto + ':' + segundos;
-    
-};
+}
 
-const data = new Date();
-var dia = data.getDay().toString().padStart(2, '0');
-var mes = data.getMonth().toString().padStart(2, '0');
+
+var data = new Date();
+var dia = data.getDate();
+var mes = data.getMonth();
+    mes += 1;
+
+var mes_ajustado = ajustaZeros(mes);
+var dia_ajustado = ajustaZeros(dia);
 var ano = data.getFullYear();
 
 
-var campo_data = document.querySelector(".campo_data");
-campo_data.textContent = (dia + '/' + mes + '/' + ano);
+const campo_data = document.querySelector(".campo_data");
+campo_data.innerHTML = dia_ajustado + '/' + (mes_ajustado) + '/' + ano;
+
+
+function ajustaZeros(data){
+    var stringData = data.toString();
+    
+    if(stringData.length == 1){
+        stringData = '0' + stringData;
+    } 
+    return stringData;
+}
