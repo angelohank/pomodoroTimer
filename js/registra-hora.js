6a4fd23fba580db4c29c7d60f6bf05479e329fc1
Angelo Hank <angelo.hank@ciss.com.br>
2023-07-13 17:06:32 -0300

    feat: removendo bulma

--- js/registra-hora.js
+++
@@ -1,7 +0,0 @@
-const botao = document.querySelector(".botao");
-
-botao.addEventListener("click", function(){
-    botao.textContent = '';
-    botao.append("Horário registrado com sucesso!");
-    botao.classList.remove("is-outlined");
-})
