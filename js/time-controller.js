3e62cc75e49331bd37edcc31d78f01aad3bb39dd
Angelo Hank <angelo.hank@ciss.com.br>
2023-07-14 09:15:56 -0300

    feat: implementando contagem regressiva e exibindo em tela

--- js/time-controller.js
+++ js/time-controller.js
@@ -1,8 +1,13 @@
+//ENUM?
+const focusTimeStep = 0;
+const shortBreakStep = 1;
+const longBreakStep = 2;
+
 const POMODORO_TIMER_DEFAULT = 1500; //25 minutos
 const SHORT_BREAK_TIMER_DEFAULT = 300; //5 minutos
 const LONG_BREAK_TIMER_DEFAULT = 600; //10 minutos
 
-let currentTime = POMODORO_TIMER_DEFAULT; //TODO: deixar dinamico de acordo com o tipo de periodo
+let currentTime = SHORT_BREAK_TIMER_DEFAULT; //TODO: deixar dinamico de acordo com o tipo de periodo
 
 const startButton = document.getElementById("startButton");
 const campo_tempo = document.getElementById("tempo-restante");
@@ -15,13 +20,34 @@ function updateCountdown() {
         //TODO: emitir notify para encerrar o periodo
     } else {
         currentTime--;
+        refreshDisplay( currentTime );
     }
 }
 
-function setDefaultValue() {
-    campo_tempo.innerHTML = POMODORO_TIMER_DEFAULT;
+function setDefaultValueInDisplay( step ) {
+    
+    let timeToDisplay = 0;
+
+    switch(step) {
+        case 0:
+            timeToDisplay = POMODORO_TIMER_DEFAULT;
+        case 1: 
+            timeToDisplay = SHORT_BREAK_TIMER_DEFAULT;
+        case 2: 
+            timeToDisplay = LONG_BREAK_TIMER_DEFAULT;
+    }
+    
+    campo_tempo.innerHTML = timeToDisplay;
 }
 
 function StartCountdown(){
     console.log("startCountdown")
+}
+
+
+function refreshDisplay( currentTime ) {
+    let min = Math.floor(currentTime / 60);
+    let seg = currentTime % 60;
+
+    campo_tempo.innerHTML = min + ":" + seg
 }
